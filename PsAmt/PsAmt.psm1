14c45d0840df93c99e61ed975f1ed510edfb28e8
Stefan Wehrli <stwehrli@gmail.com>
2015-06-22 10:16:15 +0200

    Initial commit

---
+++ PsAmt/PsAmt.psm1
@@ -0,0 +1,40 @@
+#########################################################################################
+# PsAmt Module
+# stwehrli@gmail.com
+# 28apr2014
+#########################################################################################
+
+# Contents
+# about_PsAmt
+
+#########################################################################################
+# Settings
+
+[string]$Global:AmtModulePath = Get-Module -ListAvailable PsAmt | Split-Path -Parent
+
+#########################################################################################
+
+<# 
+ .SYNOPSIS 
+  PowerShell wrapper scripts to access Amazon Mechanical Turk .Net API
+
+ .DESCRIPTION
+  PowerShell wrapper scripts to access Amazon Mechanical Turk .Net API
+    
+ .LINK
+  http://www.mturk.com
+  http://mturkdotnet.codeplex.com/
+#>
+function about_PsAmt {}
+
+#########################################################################################
+# Exports 
+Export-ModuleMember about_PsAmt
+
+# AmtUtil
+# done in AmtUtil.ps1
+
+# AmtApi Exports
+# done in AmtApi.ps1
+
+#########################################################################################
