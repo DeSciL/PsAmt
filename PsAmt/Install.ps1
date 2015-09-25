5e6eec064f78291308a944e174d64e56f84ab7fc
Stefan Wehrli <stwehrli@gmail.com>
2015-09-25 13:07:13 +0200

    Testing Install

---
+++ PsAmt/Install.ps1
@@ -0,0 +1,15 @@
+#########################################################################################
+# PsAmt Module - Utility Functions
+# stwehrli@gmail.com
+# 20sept2015
+#########################################################################################
+
+# iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/descil/psamt/psamt/install.ps1'))
+
+function Install-PsAmt {
+	Write-Host "Install PsAmt now."
+}
+
+Install-PsAmt
+
+#########################################################################################
